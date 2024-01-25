781a4ea05b133bc0f7507f04eee707fe1b10de4b
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2024-01-25 22:34:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your code here
+const button = document.getElementById("add")
+const total = document.getElementById("total")
+let sum = 0;
+button.addEventListener("click", (e)=>{
+
+const item = document.getElementById("item-name-input").value
+const price = document.getElementById("item-price-input").value
+
+
+if (item === "" && price==="") {
+    alert("Please enter a task!");
+    return;
+}
+
+if (item && price) {
+    const table = document.getElementById("todoTable").getElementsByTagName('tbody')[0];
+
+    table.innerHTML+=`
+    <td id="item">${item}</td>
+    <td id="price">${price}</td>
+    `
+    sum+=parseInt(price)
+    total.innerHTML=sum;
+    document.getElementById("item-name-input").value = "";
+    document.getElementById("item-price-input").value = "";
+}
+})
